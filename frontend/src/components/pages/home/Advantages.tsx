a7fa5afbf8ff71c9003952dccd23f3e5b7a64781
Roman Sosnovich <sosnovichroman@gmail.com>
2025-01-13 23:23:03 +0300

    advantages draft

--- frontend/src/components/pages/home/Advantages.tsx
+++ frontend/src/components/pages/home/Advantages.tsx
@@ -1,3 +1,45 @@
+import { Scroll, ShieldCheck, Truck } from 'lucide-react'
+
 export default function Advantages() {
-	return <div>Enter</div>
+	return (
+		<section className='py-15'>
+			<div className='container'>
+				<h2 className='heading-2 text-center'>
+					Почему выбитают постеры Melpomena?
+				</h2>
+				<div className='grid grid-cols-3 gap-[30px] mt-16'>
+					<div className='space-y-5 flex flex-col items-center'>
+						<Scroll strokeWidth={1} className='h-16 w-16' />
+						<h3 className='heading-4 text-main text-center'>
+							Качественные материалы
+						</h3>
+						<p className='text-body text-center'>
+							Мы используем высококачественные бумагу и чернила, благодаря чему
+							наши постеры остаются яркими спустя года.
+						</p>
+					</div>
+					<div className='space-y-5 flex flex-col items-center'>
+						<ShieldCheck strokeWidth={1} className='h-16 w-16' />
+						<h3 className='heading-4 text-main text-center'>
+							Гарантия защиты от повреждений
+						</h3>
+						<p className='text-body text-center'>
+							Мы отправляем все постеры в защитной упаковке. Если Ваш постер
+							прибудет поврежденным, свяжитесь с нами, и мы быстро решим эту
+							проблему.
+						</p>
+					</div>
+					<div className='space-y-5 flex flex-col items-center'>
+						<Truck strokeWidth={1} className='h-16 w-16' />
+						<h3 className='heading-4 text-main text-center'>
+							Доставка по всей Беларуси
+						</h3>
+						<p className='text-body text-center'>
+							Мы доставим Ваш заказ в любую точку Беларуси в кратчайшие сроки.
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
