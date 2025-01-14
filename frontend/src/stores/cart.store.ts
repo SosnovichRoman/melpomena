285c64a9295d58b02fbeaf4258efd61b640794da
Roman Sosnovich <sosnovichroman@gmail.com>
2025-01-14 22:47:30 +0300

    persist zustand

--- frontend/src/stores/cart.store.ts
+++ frontend/src/stores/cart.store.ts
@@ -1,6 +1,6 @@
-import cartService from '@/services/cart.service'
 import { ICartItem } from '@/types/cart.types'
 import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
 
 type Store = {
 	cartList: ICartItem[]
@@ -9,37 +9,42 @@ type Store = {
 	update: (_id: string, quantity: number) => void
 }
 
-const useCartStore = create<Store>()((set) => ({
-	cartList: cartService.getAll(),
-	add: (_id: string) =>
-		set((state) => {
-			const newState = {
-				cartList: [...state.cartList, { _id, quantity: 1 }],
-			}
-			cartService.save(newState.cartList)
-			return newState
-		}),
-
-	delete: (_id: string) =>
-		set((state) => {
-			const newState = {
-				cartList: state.cartList.filter((item) => item._id != _id),
-			}
-			cartService.save(newState.cartList)
-			return newState
-		}),
-
-	update: (_id: string, quantity: number) =>
-		set((state) => {
-			const newState = {
-				cartList: state.cartList.map((item) => {
-					if (item._id == _id) return { _id, quantity }
-					return item
+const useCartStore = create<Store>()(
+	persist(
+		(set) => ({
+			cartList: [],
+			add: (_id: string) =>
+				set((state) => {
+					const newState = {
+						cartList: [...state.cartList, { _id, quantity: 1 }],
+					}
+					return newState
 				}),
-			}
-			cartService.save(newState.cartList)
-			return newState
+
+			delete: (_id: string) =>
+				set((state) => {
+					const newState = {
+						cartList: state.cartList.filter((item) => item._id != _id),
+					}
+					return newState
+				}),
+
+			update: (_id: string, quantity: number) =>
+				set((state) => {
+					const newState = {
+						cartList: state.cartList.map((item) => {
+							if (item._id == _id) return { _id, quantity }
+							return item
+						}),
+					}
+					return newState
+				}),
 		}),
-}))
+		{
+			name: 'cart-storage', // name of the item in the storage (must be unique)
+			storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
+		}
+	)
+)
 
 export default useCartStore
